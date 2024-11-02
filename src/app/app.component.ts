a5eaf95009774bce50f6c7283806eb590d3f8df5
abanoub magdi <abanoubmagdi.4040@gmail.com>
2024-11-02 15:01:17 +0200

    create the cart style **  and API configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'E-Shop';
 }
+
+///////////////////////////////////////
+// import { Component, ViewEncapsulation } from '@angular/core';
+
+// @Component({
+//   selector: 'app-your-component',
+//   templateUrl: './your-component.component.html',
+//   styleUrls: ['./your-component.component.css'],
+//   encapsulation: ViewEncapsulation.None
+// })
--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.scss'
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
+products:any[]=[];
 
+  constructor(private service: ProductsService) { }
+
+  ngOnInit(){
+    this.getProducts()
+}
+getProducts(){
+  this.service.getAllProducts().subscribe((items: any) => {
+   //here i put the data in array with name products and i 'll loop by it in the html page
+    this.products=items;
+    
+    //to check that data is reurned
+    console.log(items);
+  })
+}
 }
