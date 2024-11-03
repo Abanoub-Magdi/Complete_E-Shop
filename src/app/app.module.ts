12b38cd00900a89c39f6f4633da8360b5e8d7d64
abanoub <abanob.yakob@etisalat.com>
2024-11-03 14:53:50 +0200

    configuration done successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsModule } from './products/products.module';
+import { CartsModule } from './carts/carts.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,12 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    CommonModule,
+    ProductsModule,
+    CartsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -4,23 +4,24 @@ import { ProductsService } from '../../services/products.service';
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
-  styleUrl: './all-products.component.scss'
+  styleUrls: ['./all-products.component.scss'] // Corrected property name
 })
 export class AllProductsComponent implements OnInit {
-products:any[]=[];
+  products: any[] = [];
 
   constructor(private service: ProductsService) { }
 
-  ngOnInit(){
-    this.getProducts()
-}
-getProducts(){
-  this.service.getAllProducts().subscribe((items: any) => {
-   //here i put the data in array with name products and i 'll loop by it in the html page
-    this.products=items;
-    
-    //to check that data is reurned
-    console.log(items);
-  })
-}
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.service.getAllProducts().subscribe((items: any) => {
+      // Here I put the data in an array named products and I'll loop through it in the HTML page
+      this.products = items;
+
+      // To check that data is returned
+      console.log(items);
+    });
+  }
 }
